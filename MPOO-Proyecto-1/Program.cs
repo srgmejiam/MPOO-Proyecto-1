ba54e7607fec7ae8e9bec63771d4b915e935233d
Marvin Antonio Mejia Espinoza <SrgMejia@icloud.com>
2023-05-20 12:00:28 -0600

    ejercicio

--- MPOO-Proyecto-1/Program.cs
+++ MPOO-Proyecto-1/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hola Mundo");
+﻿void NombredelMetodo()
+{
+    Console.WriteLine("Cual es tu nombre");
+    string Nombre = string.Empty;
+
+    Nombre = Console.ReadLine();
+    if (Nombre != null)
+    {
+        Console.WriteLine("Hola -" + Nombre + "\n");
+    }
+
+    for (int i = 0; i < 100; i++)
+    {
+        Console.WriteLine(i + 1);
+    }
+}
+    
+NombredelMetodo();
+
